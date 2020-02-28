6422338cda0ae9dc791cd9f42bff4002d5d015ad
Pumpkin <amdedovich@yandex.ru>
2020-02-28 23:40:35 +0300

    №3,4

--- lab1.py
+++ lab1.py
@@ -2,19 +2,21 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 from matplotlib import axes
+import numpy as np
 
+figSize = (10, 5)
 
 # конвертируем строку в список
 def convertStrToList(value):
     return list(map(int, data.split()))
 
 
-# создаем мапу значений против их количества	
+# создаем мапу значений против их количества
 def buildMapOfValueVsAmount(convertedData):
     return {value: convertedData.count(value) for value in convertedData}
 
 
-# находим уникальные значения из списка	
+# находим уникальные значения из списка
 def findUniqueValues(convertedData):
     return list(set(convertedData))
 
@@ -44,14 +46,14 @@ def plotEmpiricalDistributionFunction(uniqueValues, mapOfValueVsAmount, length):
         temp += mapOfValueVsAmount[i]
         empiricalDistributionFunctionData.append(temp / length)
     plt.figure(0)
-    plt.rcParams["figure.figsize"] = (20, 10)
+    plt.rcParams["figure.figsize"] = figSize
     printStepPlot(uniqueValues, empiricalDistributionFunctionData)
 
 
-# строим гистограмму	
+# строим гистограмму
 def plotBarChart(convertedData):
     plt.figure(1)
-    plt.rcParams["figure.figsize"] = (20, 10)
+    plt.rcParams["figure.figsize"] = figSize
 
     k = -1
     res = []
@@ -68,7 +70,7 @@ def plotBarChart(convertedData):
 
 
 # находим среднее значение (сумма всех значений из сета поделить на количество)
-def findAndPrintMiddleValue(convertedData):
+def findAndPrintMiddleValue(convertedData, print=lambda *x: print(*x)):
     mid = sum(convertedData) / len(convertedData)
     print("Среднее значаение:", mid)
     return mid
@@ -76,25 +78,25 @@ def findAndPrintMiddleValue(convertedData):
 
 # находим выборочный момент
 def moment(i, uniqueValues, convertedData):
-    return sum(map(lambda uniqueValues: (uniqueValues - mid) ** i, convertedData)) / len(convertedData)
+    return sum(map(lambda value: (value - mid) ** i, convertedData)) / len(convertedData)
 
 
 # находим выборочную дисперсию ( момент от 2)
-def findAndPrintSampleVariance(uniqueValues, convertedData):
+def findAndPrintSampleVariance(uniqueValues, convertedData, print=lambda *x: print(*x)):
     sampleVariance = moment(2, uniqueValues, convertedData)
     print("Выборочная дисперсия:", sampleVariance)
     return sampleVariance
 
 
 # находим стандартную ошибку (квадрат выб. дисперсии деленный на длину сета)
-def findAndPrintStandardError(sampleVariance, convertedData):
+def findAndPrintStandardError(sampleVariance, convertedData, print=lambda *x: print(*x)):
     standardError = (sampleVariance / len(convertedData)) ** 0.5
     print("Стандартная ошибка выборки:", standardError)
     return standardError
 
 
 # находим моду (самое часто попадающееся значение в сете)
-def findAndPrintModa(convertedData):
+def findAndPrintModa(convertedData, print=lambda *x: print(*x)):
     row = buildMapOfValueVsAmount(convertedData)
     mx = max(row.values())
     moda = 0
@@ -107,7 +109,7 @@ def findAndPrintModa(convertedData):
 
 
 # средний элемент (медиана)
-def findAndPrintMediana(convertedData):
+def findAndPrintMediana(convertedData, print=lambda *x: print(*x)):
     med = convertedData[len(convertedData) // 2]
     if len(convertedData) % 2 == 0:
         med += convertedData[len(convertedData) // 2 - 1]
@@ -117,7 +119,7 @@ def findAndPrintMediana(convertedData):
 
 
 # квартиль 1 (первый элемент делящий сет на 4 части)
-def findAndPrintQuart1(convertedData):
+def findAndPrintQuart1(convertedData, print=lambda *x: print(*x)):
     quart1 = convertedData[len(convertedData) // 4]
     if len(convertedData) % 4 == 0:
         quart1 += convertedData[len(convertedData) // 4 - 1]
@@ -126,48 +128,138 @@ def findAndPrintQuart1(convertedData):
     return quart1
 
 
-# квартиль 2 (первый элемент делящий сет на 4 части)
-def findAndPrintQuart2(convertedData):
-    quart2 = convertedData[len(convertedData) // 4 * 3]
+# квартиль 3 (первый элемент делящий сет на 4 части)
+def findAndPrintQuart3(convertedData, print=lambda *x: print(*x)):
+    quart3 = convertedData[len(convertedData) // 4 * 3]
     if len(convertedData) % 4 == 0:
-        quart2 += convertedData[len(convertedData) // 4 * 3 - 1]
-        quart2 /= 2
-    print("Квартиль 2:", quart2)
-    return quart2
+        quart3 += convertedData[len(convertedData) // 4 * 3 - 1]
+        quart3 /= 2
+    print("Квартиль 2:", quart3)
+    return quart3
 
 
 # стандартное отклонение (корень из дисперсии)
-def findAndPrintSigma(sampleVariance):
+def findAndPrintSigma(sampleVariance, print=lambda *x: print(*x)):
     sigma = sampleVariance ** 0.5
     print("Среднее стандартное отклонение:", sigma)
     return sigma
 
 
+def findAndPrintNormSigma(sampleVariance, len):
+    normSigma = (len * sampleVariance / (len - 1)) ** 0.5
+    print("Исправленное отклонение:", normSigma)
+    return normSigma
+
+
 # находим эксцесс (мера остроты пика в распределении случайной величины)
-def findAndPrintExcess(uniqueValues, convertedData, sampleVariance):
+def findAndPrintExcess(uniqueValues, convertedData, sampleVariance, print=lambda *x: print(*x)):
     excess = moment(4, uniqueValues, convertedData) / (sampleVariance ** 2) - 3
     print("Эксцесс:", excess)
     return excess
 
 
-# находим ассиметрию (числовое отображение степени отклонения графика распределения показателей от симметричного графика распределения) 
-def findAndPrintAsymmetry(uniqueValues, convertedData, sigma):
+# находим ассиметрию (числовое отображение степени отклонения графика распределения показателей от симметричного графика распределения)
+def findAndPrintAsymmetry(uniqueValues, convertedData, sigma, print=lambda *x: print(*x)):
     asymmetry = moment(3, uniqueValues, convertedData) / (sigma ** 3)
     print("Ассиметричность:", asymmetry)
     return asymmetry
 
 
 # находим мак и мин
-def findAndPrintMaxMin(convertedData):
+def findAndPrintMaxMin(convertedData, print=lambda *x: print(*x)):
     print("Минимум:", convertedData[0])
     print("Максимум:", convertedData[-1])
     return convertedData[-1], convertedData[0]
 
 
+def gaussian(x, mu, sig):
+    return 1. / (np.sqrt(2. * np.pi)) * np.exp(-np.power((x - mu) / sig, 2.) / 2)
+
+
+# находим теоритическую частоту
+def findAndPrintTheoreticalFrequency(mapOfValueVsAmount, len, m, sigma):
+    print("Теоритические частоты:")
+    return {value: len * gaussian(value, m, sigma) / sigma for value in mapOfValueVsAmount.keys()}
+
+
+# находим эмпирическое значение критерия Пирсона
+def findAndPrintEmpiricalMeaningsPearson(mapOfValueVsAmount, theoreticalFrequency):
+    empiricalMeanings = sum(
+        (i[0] - i[1]) ** 2 / i[1] for i in zip(mapOfValueVsAmount.values(), theoreticalFrequency.values()))
+    print("Эмпирическое значение критерия Пирсона:", empiricalMeanings)
+    return empiricalMeanings
+
+
+# находим число степеней свободы
+def findAndPrintDegreeOfFreedom(mapOfValueVsAmount, r):
+    s = len(mapOfValueVsAmount.keys()) - 1 - r
+    print("Степень свободы:", s)
+    return s
+
+
+def cut(map):
+    return {key: round(map[key], 2) for key in map}
+
+
+def printFrequency(data):
+    print('{:>2}'.format("xi"), end="")
+    for i in data.keys():
+        print('{:>5}'.format(str(i)), end="")
+    print()
+    print('{:>2}'.format("mi"), end="")
+    for i in data.values():
+        print('{:>5}'.format(str(i)), end="")
+    print()
+    print()
+
+
 # находим критерий согласия Пирсона
-def findAndPrintAcceptanceCriterion(convertedData):
-    print("---Критерий Пирсона---")
-    print("Объем выборки: ", len(convertedData))
+def findAndPrintAcceptanceCriterion(convertedData, m, sigma):
+    print("\n\n---Критерий Пирсона---\n\n")
+    mapOfValueVsAmount = buildMapOfValueVsAmount(convertedData)
+    print()
+    printFrequency(mapOfValueVsAmount)
+
+    theoreticalFrequency = findAndPrintTheoreticalFrequency(buildMapOfValueVsAmount(convertedData), len(convertedData),
+                                                            m, sigma)
+    printFrequency(cut(theoreticalFrequency))
+
+    empiricalMeanings = findAndPrintEmpiricalMeaningsPearson(mapOfValueVsAmount, theoreticalFrequency)
+    print(
+        "Т.к. степень свободы зависит от 2-х параметров, то число степеней свободы будет равно (количество выборов - 1 - количество параметров от которых зависит)")
+    dof = findAndPrintDegreeOfFreedom(buildMapOfValueVsAmount(convertedData), 2)
+    print("Критическое значение критерия Пирсона: 64,201")
+    print("Наблюдаемое значение критерия Пирсона меньше критического, следовательно закон принимаем")
+
+
+def findAndPrintInterval(convertedData, mid, sampleVariance):
+    print("\n\n---Доверительные интервалы---\n\n")
+    print("\n---Доверительный интервал для мат. ожидания---\n")
+    normSigma = findAndPrintNormSigma(sampleVariance, len(convertedData))
+
+    z = 1.984217
+    eps = z * normSigma / (len(convertedData) ** 0.5)
+    print("Надежность 0.95")
+    # print( f"Коэффициент для уровня значимости 0.05 равно {z} (уровень значимости = 1-надежность)")
+    print("Первая граница:", mid - eps)
+    print("Вторая граница:", mid + eps)
+
+    print("\n---Доверительный интервал для среднего квадратичного отклонения---\n")
+
+    '''sigma = findAndPrintSigma(findAndPrintSampleVariance(findUniqueValues(convertedData), convertedData))
+    q = 0.143
+    print("Надежность 0.95")
+    print("Значение интегральной функции Лапласа при надежности 0.95 равно 0.143")
+
+    print("Первая граница:", sigma * (1 - q))
+    print("Вторая граница:", sigma * (1 + q))'''
+
+    # a1 = 0.025, a2 = 0.975, k = 99,
+    xi1 = 128.4219886
+    xi2 = 73.36108019
+    print("Первая граница:", normSigma * (((len(convertedData) - 1) / xi1) ** 0.5))
+    print("Вторая граница:", normSigma * (((len(convertedData) - 1) / xi2) ** 0.5))
+
 
 
 data = '''189 207 213 208 186 210 198 219 231 227 202 211 220 236 227 220 210 183 213
@@ -185,7 +277,6 @@ uniqueValues = findUniqueValues(convertedData)
 
 plotEmpiricalDistributionFunction(uniqueValues, mapOfValueVsAmount, length)
 plotBarChart(convertedData)
-plt.show()
 
 mid = findAndPrintMiddleValue(convertedData)
 sampleVariance = findAndPrintSampleVariance(uniqueValues, convertedData)
@@ -193,8 +284,14 @@ standardError = findAndPrintStandardError(sampleVariance, convertedData)
 moda = findAndPrintModa(convertedData)
 med = findAndPrintMediana(convertedData)
 quart1 = findAndPrintQuart1(convertedData)
-quart2 = findAndPrintQuart2(convertedData)
+quart3 = findAndPrintQuart3(convertedData)
 sigma = findAndPrintSigma(sampleVariance)
 excess = findAndPrintExcess(uniqueValues, convertedData, sampleVariance)
 asymetry = findAndPrintAsymmetry(uniqueValues, convertedData, sigma)
 max, min = findAndPrintMaxMin(convertedData)
+plt.figure(2)
+plt.rcParams["figure.figsize"] = figSize
+plt.boxplot([min, quart1, med, quart3, max])
+plt.show()
+findAndPrintAcceptanceCriterion(convertedData, mid, sigma)
+findAndPrintInterval(convertedData, mid, sampleVariance)
